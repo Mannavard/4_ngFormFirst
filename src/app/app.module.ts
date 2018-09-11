a86da9a03f595571a755f8dcfd8961473537d494
Mannavard <mannavard.ak@gmail.com>
2018-09-11 18:18:57 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormSimpleComponent } from './form-simple/form-simple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSimpleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-simple/form-simple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-simple',
+  templateUrl: './form-simple.component.html',
+  styleUrls: ['./form-simple.component.css']
+})
+export class FormSimpleComponent {
+
+}
